f1e58e30f929b956a835c7c4eeaa40c4403e828b
kevinderitis <32942115+kevinderitis@users.noreply.github.com>
2020-11-02 17:14:59 -0300

    Modificaciones en list coins

--- src/controllers/coins.js
+++ src/controllers/coins.js
@@ -4,9 +4,29 @@ const { response } = require('express');
 
 
 async function coinList() {
-    let res = await fetch('https://api.coingecko.com/api/v3/coins/list');
-    let result = await res.json();
-    return result;
+    let data = [];
+    try {
+        let res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+        let result = await res.json();
+
+        result.forEach(element => {
+
+            let file = {
+                "symbol": element.symbol,
+                "current_price": element.current_price,
+                "name": element.name,
+                "image": element.image,
+                "last_updated": element.last_updated
+            }
+
+            data.push(file);
+
+        });
+    } catch (error) {
+        return { "rc": 6, "msg": "API connection error" }
+    }
+
+    return data;
 }
 
 function coinTop() {
