917f5f565ab11d324f7fe82a89741098adecec69
kevinderitis <32942115+kevinderitis@users.noreply.github.com>
2020-11-04 16:45:10 -0300

    Modificacion test unit

--- spec/models/auth_test.spec.js
+++ spec/models/auth_test.spec.js
@@ -1,8 +1,9 @@
 const auth = require('../../src/controllers/auth');
+const { user } = require('../../src/database');
 
 beforeEach(async function () {
     const user = {
-        "id": "1",
+        "id": "99",
         "name": "admin",
         "lastname": "admin",
         "username": "admin",
@@ -15,5 +16,19 @@ beforeEach(async function () {
 
 
 it('Testing signin auth', async function () {
-   
+    expusers = await user.findAll({
+        where: { "id": 99 }
+    });
+    expect('admin').toBe(expusers[0].name);
+});
+
+it('Testing login auth', async function () {
+    token = await auth.login('admin', 'admin');
+    expect(token).toBeDefined();
+});
+
+afterEach(async function () {
+    await user.destroy({
+        where: { "id": 99 }
+    });
 });
